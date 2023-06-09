15880824ac582d9ad63eabc015469358002581af
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-08 23:46:06 -0700

    about me layout complete

--- src/App.js
+++ src/App.js
@@ -24,23 +24,23 @@ function App() {
           <img className="image" src={image} alt="jess" />
         </div>
       </section>
+
       <section className="about-me-page ">
         <h1>About Me</h1>
         <div className="about-me-content">
-          <div className="about-me-paragraph">
-            <p>
-              My first coding experience was using Python to run automated
-              performance verifications for calibration test equipment. It
-              piqued my interest so much that I decided to attend a coding
-              bootcamp and change my career path. I have experience building
-              RESTful APIs with Node, Express, and Django. I'm comfortable with
-              integrating APIs into the frontend, making third-party API calls,
-              and creating responsive UIs. Currently, one of my main interests
-              has been in Web3. I have created smart contracts and decentralized
-              applications (DApps) on the Ethereum blockchain using frameworks
-              like Ethers.js, Solidity, and Hardhat.
-            </p>
-          </div>
+          <p className="about-me-p">
+            My first coding experience was using Python to run automated
+            performance verifications for calibration test equipment. It piqued
+            my interest so much that I decided to attend a coding bootcamp and
+            change my career path. I have experience building RESTful APIs with
+            Node, Express, and Django. I'm comfortable with integrating APIs
+            into the frontend, making third-party API calls, and creating
+            responsive UIs. Currently, one of my main interests has been in
+            Web3. I have created smart contracts and decentralized applications
+            (DApps) on the Ethereum blockchain using frameworks like Ethers.js,
+            Solidity, and Hardhat.
+          </p>
+
           <div className="tech-stack">
             <FontAwesomeIcon
               icon={faHtml5}
@@ -85,10 +85,18 @@ function App() {
             href="https://www.linkedin.com/in/jesswritincode/"
             className="linkedin"
           >
-            <FontAwesomeIcon icon={faLinkedin} style={{ color: '#155fd5' }} />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="2xl"
+              style={{ color: '#155fd5' }}
+            />
           </a>
           <a href="https://github.com/justbytes" className="github">
-            <FontAwesomeIcon icon={faGithub} style={{ color: '#000000' }} />
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="2xl"
+              style={{ color: '#000000' }}
+            />
           </a>
         </div>
       </section>
