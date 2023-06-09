065f0d5a24cbb87fd26b876d7837ffcca6e2826c
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-08 22:52:44 -0700

    about me content filled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import react from 'react';
-import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLinkedin,
+  faGithub,
+  faReact,
+  faNode,
+  faPython,
+  faBootstrap,
+  faJs,
+  faHtml5,
+  faCss3Alt,
+} from '@fortawesome/free-brands-svg-icons';
 import image from './images/profile.jpg';
 
 function App() {
@@ -16,9 +25,71 @@ function App() {
         </div>
       </section>
       <section className="about-me-page ">
-        <div>
-          <h1>About Me</h1>
-          <p>yatyatayatyatyatyaytyatyaytyatyaytyatyaytyatyaytyaytyatyaytyaty</p>
+        <h1>About Me</h1>
+        <div className="about-me-content">
+          <div className="about-me-paragraph">
+            <p>
+              My first coding experience was using Python to run automated
+              performance verifications for calibration test equipment. It
+              piqued my interest so much that I decided to attend a coding
+              bootcamp and change my career path. I have experience building
+              RESTful APIs with Node, Express, and Django. I'm comfortable with
+              integrating APIs into the frontend, making third-party API calls,
+              and creating responsive UIs. Currently, one of my main interests
+              has been in Web3. I have created smart contracts and decentralized
+              applications (DApps) on the Ethereum blockchain using frameworks
+              like Ethers.js, Solidity, and Hardhat.
+            </p>
+          </div>
+          <div className="tech-stack">
+            <FontAwesomeIcon
+              icon={faHtml5}
+              size="2xl"
+              style={{ color: '#ff5900' }}
+            />
+            <FontAwesomeIcon
+              icon={faCss3Alt}
+              size="2xl"
+              style={{ color: '#1868f2' }}
+            />
+            <FontAwesomeIcon
+              icon={faJs}
+              size="2xl"
+              style={{ color: '#ecf005' }}
+            />
+            <FontAwesomeIcon
+              icon={faPython}
+              size="2xl"
+              style={{ color: '#b1c421' }}
+            />
+            <FontAwesomeIcon
+              icon={faReact}
+              size="2xl"
+              style={{ color: '#2ab2df' }}
+            />
+            <FontAwesomeIcon
+              icon={faNode}
+              size="2xl"
+              style={{ color: '#2a933b' }}
+            />
+
+            <FontAwesomeIcon
+              icon={faBootstrap}
+              size="2xl"
+              style={{ color: '#821fb7' }}
+            />
+          </div>
+        </div>
+        <div className="links">
+          <a
+            href="https://www.linkedin.com/in/jesswritincode/"
+            className="linkedin"
+          >
+            <FontAwesomeIcon icon={faLinkedin} style={{ color: '#155fd5' }} />
+          </a>
+          <a href="https://github.com/justbytes" className="github">
+            <FontAwesomeIcon icon={faGithub} style={{ color: '#000000' }} />
+          </a>
         </div>
       </section>
       <section className="projects-page">
