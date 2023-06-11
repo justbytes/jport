fe18b5d92af5911cea3b449b30c1adc8e10ef62d
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-10 17:51:38 -0700

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
+import React, { useState } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLinkedin,
-  faGithub,
-  faReact,
-  faNode,
-  faPython,
-  faBootstrap,
-  faJs,
-  faHtml5,
-  faCss3Alt,
-} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import image from './images/profile.jpg';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isToggled, setIsToggled] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsToggled(!isToggled);
+  };
+
+  const handleMouseEnter = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsSidebarOpen(false);
+    if (isToggled) setIsSidebarOpen(true);
+  };
   return (
     <>
       <section className="landing-page">
         <div className="name-div">
           <h1>Jesse Marino</h1>
-          <p>Full-Stack Developer</p>
+          <p>Full Stack Developer</p>
         </div>
         <div className="image-div">
           <img className="image" src={image} alt="jess" />
         </div>
       </section>
 
-      <section className="about-me-page ">
+      <section className="about-me-page">
         <h1>About Me</h1>
         <div className="about-me-content">
           <p className="about-me-p">
@@ -34,7 +41,7 @@ function App() {
             my interest so much that I decided to attend a coding bootcamp and
             change my career path. I have experience building RESTful APIs with
             Node, Express, and Django. I'm comfortable with integrating APIs
-            into the frontend, making third-party API calls, and creating
+            into the front end, making third-party API calls, and creating
             responsive UIs. Currently, one of my main interests has been in
             Web3. I have created smart contracts and decentralized applications
             (DApps) on the Ethereum blockchain using frameworks like Ethers.js,
@@ -42,42 +49,14 @@ function App() {
           </p>
 
           <div className="tech-stack">
-            <FontAwesomeIcon
-              icon={faHtml5}
-              size="2xl"
-              style={{ color: '#ff5900' }}
-            />
-            <FontAwesomeIcon
-              icon={faCss3Alt}
-              size="2xl"
-              style={{ color: '#1868f2' }}
-            />
-            <FontAwesomeIcon
-              icon={faJs}
-              size="2xl"
-              style={{ color: '#ecf005' }}
-            />
-            <FontAwesomeIcon
-              icon={faPython}
-              size="2xl"
-              style={{ color: '#b1c421' }}
-            />
-            <FontAwesomeIcon
-              icon={faReact}
-              size="2xl"
-              style={{ color: '#2ab2df' }}
-            />
-            <FontAwesomeIcon
-              icon={faNode}
-              size="2xl"
-              style={{ color: '#2a933b' }}
-            />
-
-            <FontAwesomeIcon
-              icon={faBootstrap}
-              size="2xl"
-              style={{ color: '#821fb7' }}
-            />
+            <button
+              className="sidebar-button"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              onClick={toggleSidebar}
+            >
+              See Tech Stack
+            </button>
           </div>
         </div>
         <div className="links">
@@ -98,6 +77,37 @@ function App() {
               style={{ color: '#000000' }}
             />
           </a>
+        </div>
+        <div className="sidebar-container">
+          <div className={`sidebar ${isSidebarOpen ? 'show' : 'hidden'}`}>
+            <div className="sidebar-content">
+              <h2>Tech Stack</h2>
+              <h3>Front End</h3>
+              <ul>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>jQuery</li>
+                <li>React.js</li>
+                <li>Handlebars.js</li>
+                <li>Bootstrap</li>
+              </ul>
+              <h3>Back End</h3>
+              <ul>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>Sequelize</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Python</li>
+                <li>Django</li>
+                <li>Flask</li>
+                <li>Solidity</li>
+                <li>Ethers.js</li>
+                <li>Hardhat</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
       <section className="projects-page">
